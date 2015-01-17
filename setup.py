6c0ef4c16ac80ea88461d4f368131d97f420843d
MrTango <md@derico.de>
2015-01-17 12:47:37 +0100

    add pytest to test extras_require

--- setup.py
+++ setup.py
@@ -74,8 +74,13 @@ setup(
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
+        'dev':[
+            'check-manifest'
+        ],
+        'test':[
+            'coverage',
+            'pytest',
+        ],
     },
 
     # If there are data files included in your packages that need to be
