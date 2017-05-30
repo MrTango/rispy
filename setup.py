4ad9256fc39dad7970100a380dd83d87b33a2d70
Andy Shapiro <shapiromatron@gmail.com>
2017-05-29 22:48:12 -0400

    remove init in non-module

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def find_version():
                                   version_file, re.M)
         if version_match:
             return version_match.group(1)
-        raise RuntimeError("Unable to find version string.")
+        raise RuntimeError('Unable to find version string.')
 
 setup(
     name='RISparser',
