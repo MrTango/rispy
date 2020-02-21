c16579ea54429ce7dda4ef33688ccf4aa4a8dc3c
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-02-21 20:21:43 +0100

    Replace N2 'abstract' by 'notes_abstract' to avoid conflicts (#16)

--- RISparser/config.py
+++ RISparser/config.py
@@ -50,7 +50,7 @@ TAG_KEY_MAPPING = {
     'M1': 'note',
     'M3': 'type_of_work',
     'N1': 'notes',  # ListType
-    'N2': 'abstract',
+    'N2': 'notes_abstract',
     'NV': 'number_of_volumes',
     'OP': 'original_publication',
     'PB': 'publisher',
