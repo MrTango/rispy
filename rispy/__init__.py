0edf42161daa6e3e1df1823d9d957212d820dee7
Andy Shapiro <shapiromatron@gmail.com>
2020-02-22 09:21:29 -0500

    update version for next release

--- rispy/__init__.py
+++ rispy/__init__.py
@@ -1,6 +1,6 @@
 """Read and write WOK and Refman's RIS files"""
 
-__version__ = "0.4.3-next"
+__version__ = "0.5.0"
 
 from .config import LIST_TYPE_TAGS, TAG_KEY_MAPPING  # noqa
 from .parser import load, loads  # noqa
