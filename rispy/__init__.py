fa4e280cabc7b2fe828fd55cf3ed11353648b3e2
KOLANICH <kolan_n@mail.ru>
2020-02-27 13:39:52 +0300

    Moved the metadata into setup.cfg

--- rispy/__init__.py
+++ rispy/__init__.py
@@ -1,7 +1,6 @@
 """Read and write RIS and WOK files"""
 
-__version__ = "0.5.0"
-
+from .version import __version__
 from .config import LIST_TYPE_TAGS, TAG_KEY_MAPPING  # noqa
 from .parser import RisImplementation, load, loads  # noqa
 from .writer import dump, dumps  # noqa
