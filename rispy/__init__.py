aea55fd8d8b9f77ac4d7c24a84b62807aba5400f
Andy Shapiro <shapiromatron@gmail.com>
2023-07-13 13:58:07 -0400

    revert version change

--- rispy/__init__.py
+++ rispy/__init__.py
@@ -1,10 +1,10 @@
 """A Python reader/writer of RIS reference files"""
 
 from .config import LIST_TYPE_TAGS, TAG_KEY_MAPPING, TYPE_OF_REFERENCE_MAPPING
-from .parser import load, loads, BaseParser, RisParser, WokParser
-from .writer import dump, dumps, BaseWriter, RisWriter
+from .parser import BaseParser, RisParser, WokParser, load, loads
+from .writer import BaseWriter, RisWriter, dump, dumps
 
-__version__ = "0.8.0"
+__version__ = "0.7.1"
 
 __all__ = [
     "__version__",
